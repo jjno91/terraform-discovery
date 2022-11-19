e0df4fb7caf23106912670d941ad88a05984fc39
Jeremiah Norris <jjno91@gmail.com>
2022-11-18 23:15:38 -0600

    add multiple test workspaces

--- test.sh
+++ test.sh
@@ -1,3 +1,26 @@
+terraform workspace new share || terraform workspace select share
 terraform init
 terraform apply -auto-approve
 terraform plan -detailed-exitcode
+
+terraform workspace new discover || terraform workspace select discover
+terraform init
+terraform apply -auto-approve
+terraform plan -detailed-exitcode
+
+terraform workspace new solo || terraform workspace select solo
+terraform init
+terraform apply -auto-approve
+terraform plan -detailed-exitcode
+
+terraform workspace new share || terraform workspace select share
+terraform destroy -auto-approve
+terraform plan -detailed-exitcode -destroy
+
+terraform workspace new discover || terraform workspace select discover
+terraform destroy -auto-approve
+terraform plan -detailed-exitcode -destroy
+
+terraform workspace new solo || terraform workspace select solo
+terraform destroy -auto-approve
+terraform plan -detailed-exitcode -destroy
